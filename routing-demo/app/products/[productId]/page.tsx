7332804d9d26a6ecf13ac4734b8dba7ef5ac79bb
Catgirl <zeroaninea@gmail.com>
2025-10-24 21:37:28 +0400

    use params in the template

--- routing-demo/app/products/[productId]/page.tsx
+++ routing-demo/app/products/[productId]/page.tsx
@@ -1,3 +1,8 @@
-export default function ProductDetails() {
-  return <h1>Product details</h1>;
+export default async function ProductDetails({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const productId = (await params).productId;
+  return <h1>Product details {productId}</h1>;
 }
