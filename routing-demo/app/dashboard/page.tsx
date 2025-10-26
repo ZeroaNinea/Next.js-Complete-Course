38ee94d7b5a238a046c527d3a1ba34723638838c
Catgirl <zeroaninea@gmail.com>
2025-10-26 16:42:16 +0400

    LineChart imported

--- routing-demo/app/dashboard/page.tsx
+++ routing-demo/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import LineChart from './line-chart';
+
 function BarChart() {
   return <h1>Bar Chart</h1>;
 }
@@ -7,6 +9,7 @@ export default function Dashboard() {
     <>
       <h1>Dashboard</h1>
       <BarChart />
+      <LineChart />
     </>
   );
 }
