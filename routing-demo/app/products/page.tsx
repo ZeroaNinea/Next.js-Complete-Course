77aabc5dd283a630c1e1204341bef7a88ca00c6e
Catgirl <zeroaninea@gmail.com>
2025-10-27 19:54:54 +0400

    the products looped

--- routing-demo/app/products/page.tsx
+++ routing-demo/app/products/page.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 
 export default function ProductList() {
+  const products = [];
+
+  for (let i = 1; i <= 10; i++) {
+    products.push(
+      <h2 key={i}>
+        <Link href={`products/${i}`}>Product {i}</Link>
+      </h2>
+    );
+  }
+
   return (
     <>
       <Link href="/">Home</Link>
       <h1>Product list</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      {products}
     </>
   );
 }
