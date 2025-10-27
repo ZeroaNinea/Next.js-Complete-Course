dfe4d464b72f616bc0ab7f3ea6450b00a67bec6a
Catgirl <zeroaninea@gmail.com>
2025-10-28 01:17:22 +0400

    loading works

--- routing-demo/app/blog/page.tsx
+++ routing-demo/app/blog/page.tsx
@@ -1,3 +1,17 @@
-export default function Blog() {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Blog',
+  },
+};
+
+export default async function Blog() {
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve('Intentional delay.');
+    }, 2000)
+  );
+
   return <h1>Blog</h1>;
 }
