8910d90e9f7d764b53feb0be641c314d2704b76c
Catgirl <zeroaninea@gmail.com>
2025-10-27 19:03:08 +0400

    counter.tsx created

--- routing-demo/app/counter/page.tsx
+++ routing-demo/app/counter/page.tsx
@@ -1,18 +1,11 @@
 'use client';
 
-import { useState } from 'react';
+import Counter from './counter';
 
 export const metadata = {
   title: 'Counter',
 };
 
 export default function CounterPage() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </div>
-  );
+  return <Counter />;
 }
