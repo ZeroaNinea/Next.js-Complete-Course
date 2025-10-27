739b8bb3f53c1b52c52cd5c7378d53a272fd7a2a
Catgirl <zeroaninea@gmail.com>
2025-10-27 23:11:28 +0400

    params used

--- routing-demo/app/articles/[articleId]/page.tsx
+++ routing-demo/app/articles/[articleId]/page.tsx
@@ -1,15 +1,24 @@
 import Link from 'next/link';
 
-export default function NewsArticle() {
+export default async function NewsArticle({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: { lang: 'en' | 'fr' | 'es' };
+}) {
+  const { articleId } = await params;
+  const { lang } = await searchParams;
+
   return (
     <div>
-      <h1>News article id</h1>
-      <p>Reading in language</p>
+      <h1>News article {articleId}</h1>
+      <p>Reading in {lang}</p>
 
       <div className="flex gap-4 flex-col">
-        <Link href="/articles/id?lang=en">English</Link>
-        <Link href="/articles/id?lang=fr">French</Link>
-        <Link href="/articles/id?lang=es">Spanish</Link>
+        <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+        <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
+        <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
       </div>
     </div>
   );
