bb27f4d4b42520c5bae42c80721352bbe19ae212
Catgirl <zeroaninea@gmail.com>
2025-10-27 22:41:03 +0400

    articles added

---
+++ routing-demo/app/articles/[articleId]/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function NewsArticle() {
+  return (
+    <div>
+      <h1>News article id</h1>
+      <p>Reading in language</p>
+
+      <div>
+        <Link href="/articles/id?lang=en">English</Link>
+        <Link href="/articles/id?lang=fr">French</Link>
+        <Link href="/articles/id?lang=es">Spanish</Link>
+      </div>
+    </div>
+  );
+}
--- routing-demo/app/page.tsx
+++ routing-demo/app/page.tsx
@@ -80,6 +80,7 @@ export default function Home() {
       <Link href="/products">Products</Link>
       <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
       <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+      <Link href="/articles/breaking-news-123?lang=es">Read in Spanish</Link>
     </>
   );
 }
