fe7dbefcb740e7b70d4570e4c3482f361de24d64
Catgirl <zeroaninea@gmail.com>
2025-10-28 00:08:41 +0400

    button works

--- routing-demo/app/order-product/page.tsx
+++ routing-demo/app/order-product/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function OrderProduct() {
-  const handleClick = () => {};
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing order...');
+    router.push('/');
+  };
 
   return (
     <>
