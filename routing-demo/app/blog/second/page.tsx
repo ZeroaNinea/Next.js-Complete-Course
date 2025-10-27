e31235f0787478bb58e066d8a28686b705b03d00
Catgirl <zeroaninea@gmail.com>
2025-10-27 19:24:21 +0400

    title metadata tested

--- routing-demo/app/blog/second/page.tsx
+++ routing-demo/app/blog/second/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Blog', // Add absolute title.
+  },
+};
+
 export default function BlogSecond() {
   return <h1>Blog Second</h1>;
 }
