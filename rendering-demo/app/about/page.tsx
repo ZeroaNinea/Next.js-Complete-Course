a78bc198fce0a4b5d79c7b5ad9f60275c2ea79c2
Catgirl <zeroaninea@gmail.com>
2025-10-29 23:38:48 +0400

    implementing dynamic rendering

--- rendering-demo/app/about/page.tsx
+++ rendering-demo/app/about/page.tsx
@@ -1,3 +1,9 @@
-export default function About() {
+import { cookies } from 'next/headers';
+
+export default async function About() {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get('theme');
+  console.log(theme);
+
   return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
