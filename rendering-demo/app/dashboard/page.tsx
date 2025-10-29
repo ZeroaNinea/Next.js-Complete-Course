abf02ed2457f4a279f7cf63bcde8f8447b531647
Catgirl <zeroaninea@gmail.com>
2025-10-29 17:21:08 +0400

    dashboard has been implemented with state management

--- rendering-demo/app/dashboard/page.tsx
+++ rendering-demo/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 
 export default function DashboardPage() {
@@ -6,7 +8,11 @@ export default function DashboardPage() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-100 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+      />
       <p>Hello, {name}!</p>
     </div>
   );
