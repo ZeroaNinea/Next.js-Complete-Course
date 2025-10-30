a1c0e60bd97f48fd45efcf345917635a22da8543
Catgirl <zeroaninea@gmail.com>
2025-10-30 18:14:54 +0400

    headers added

--- route-handlers-demo/proxy.ts
+++ route-handlers-demo/proxy.ts
@@ -4,9 +4,20 @@ import type { NextRequest } from 'next/server';
 export function proxy(request: NextRequest) {
   // return NextResponse.redirect(new URL('/', request.url));
 
-  if (request.nextUrl.pathname === '/profile') {
-    return NextResponse.rewrite(new URL('/hello', request.nextUrl));
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get('theme')?.value;
+
+  if (!themePreference) {
+    response.cookies.set('theme', 'dark');
   }
+
+  response.headers.set('custom-header', 'custom-value');
+
+  // if (request.nextUrl.pathname === '/profile') {
+  //   return NextResponse.rewrite(new URL('/hello', request.nextUrl));
+  // }
+
+  return response;
 }
 
 // export const config = {
