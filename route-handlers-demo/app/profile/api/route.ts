7a3ef29616099ab565b8133749049ddd227b6368
Catgirl <zeroaninea@gmail.com>
2025-10-30 14:40:53 +0400

    log the access token

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,4 +1,9 @@
-export async function GET() {
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get('Authorization'));
+
   return new Response(JSON.stringify({ data: 'API Response' }), {
     headers: { 'Content-Type': 'application/json' },
     status: 200,
