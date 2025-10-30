0f731f2b459ddb10db6bbd231b7ec979ee021aa5
Catgirl <zeroaninea@gmail.com>
2025-10-30 21:39:02 +0400

    components created

---
+++ rendering-demo/app/product-reviews/page.tsx
@@ -0,0 +1,14 @@
+import Product from '@/components/product';
+import Reviews from '@/components/reviews';
+
+export default function ProductReviews() {
+  return (
+    <>
+      <div>
+        <h1>Product Reviews</h1>
+        <Product />
+        <Reviews />
+      </div>
+    </>
+  );
+}
