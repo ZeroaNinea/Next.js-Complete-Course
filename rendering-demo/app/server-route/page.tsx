4af8b9490e36e4f3f8894119bbbb8bd552bafb56
Catgirl <zeroaninea@gmail.com>
2025-10-30 22:42:07 +0400

    CSR and SSR implemented

--- rendering-demo/app/server-route/page.tsx
+++ rendering-demo/app/server-route/page.tsx
@@ -3,5 +3,9 @@ import { serverSideFunction } from '@/utils/server-utils';
 export default function ServerRoutePage() {
   const result = serverSideFunction();
 
-  return <h1>Server Route {result}</h1>;
+  return (
+    <>
+      <h1>Server Route {result}</h1>
+    </>
+  );
 }
