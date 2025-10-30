226caeaba9b95e7cd9d619ac10c6a982597936e9
Catgirl <zeroaninea@gmail.com>
2025-10-30 12:45:10 +0400

    filter the comments with params

--- route-handlers-demo/app/comments/route.ts
+++ route-handlers-demo/app/comments/route.ts
@@ -1,6 +1,16 @@
+import type { NextRequest } from 'next/server';
 import { comments } from './data';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get('query');
+
+  if (query) {
+    return Response.json(
+      comments.filter((comment) => comment.text.includes(query))
+    );
+  }
+
   return Response.json(comments);
 }
 
