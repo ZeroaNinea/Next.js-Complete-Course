ff04bbe4db7d17eee0d4d0498df30988c7869b63
Catgirl <zeroaninea@gmail.com>
2025-10-30 15:38:06 +0400

    set cookies

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,19 +1,20 @@
-// import { NextRequest } from 'next/server';
+import { NextRequest } from 'next/server';
 import { headers } from 'next/headers';
 
-export async function GET(/* request: NextRequest */) {
+export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
   // console.log(requestHeaders.get('Authorization'));
 
   const headersList = await headers();
   console.log(headersList.get('Authorization'));
+  console.log(request.cookies.get('theme')?.value);
 
   // return new Response(JSON.stringify({ data: 'API Response' }), {
   //   headers: { 'Content-Type': 'application/json' },
   //   status: 200,
   // });
   return new Response('<h1>Profile API data.</h1>', {
-    headers: { 'Content-Type': 'text/html' },
+    headers: { 'Content-Type': 'text/html', 'Set-Cookie': 'theme=dark' },
     status: 200,
   });
 }
