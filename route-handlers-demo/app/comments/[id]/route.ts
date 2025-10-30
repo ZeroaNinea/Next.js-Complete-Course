9d634752f943af24b81070083eb03894e766efc6
Catgirl <zeroaninea@gmail.com>
2025-10-30 11:39:25 +0400

    update a comment

--- route-handlers-demo/app/comments/[id]/route.ts
+++ route-handlers-demo/app/comments/[id]/route.ts
@@ -12,3 +12,20 @@ export async function GET(
     status: 200,
   });
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((c) => c.id === parseInt(id));
+  comments[index].text = text;
+
+  return new Response(JSON.stringify(comments[index]), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 200,
+  });
+}
