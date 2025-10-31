bf6d92daa4b44b120a8e4d5a88191f565babc4b2
Catgirl <zeroaninea@gmail.com>
2025-10-31 20:32:12 +0400

    a slider added

--- rendering-demo/app/client-route/page.tsx
+++ rendering-demo/app/client-route/page.tsx
@@ -1,13 +1,45 @@
 'use client';
 
+import React from 'react';
+import Slider from 'react-slick';
+import Image from 'next/image';
+
+import 'slick-carousel/silck/slick.css';
+import 'slick-carousel/silck/slick-theme.css';
+import './ImageSlider.css';
+
+import './style.css';
+
 // import { serverSideFunction } from '@/utils/server-utils';
 
 export default function ClientRoutePage() {
   // const result = serverSideFunction();
 
+  const settings = {
+    dots: true,
+  };
+
+  // return (
+  //   <>
+  //     <h1>Client Route</h1>
+  //   </>
+  // );
+
   return (
     <>
-      <h1>Client Route</h1>
+      <div className="image-slider-container">
+        <Slider {...settings}>
+          <div>
+            <Image src="http://placekitten.com/g/400/200" alt="Kitten 1" />
+          </div>
+          <div>
+            <Image src="http://placekitten.com/g/400/200" alt="Kitten 2" />
+          </div>
+          <div>
+            <Image src="http://placekitten.com/g/400/200" alt="Kitten 3" />
+          </div>
+        </Slider>
+      </div>
     </>
   );
 }
