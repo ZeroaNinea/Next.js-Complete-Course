b8539826316b8154ad87f7bf5754386f33255983
Catgirl <zeroaninea@gmail.com>
2025-11-02 02:17:47 +0400

    Promise added

--- data-fetching-demo/app/posts-sequential/author.tsx
+++ data-fetching-demo/app/posts-sequential/author.tsx
@@ -4,6 +4,7 @@ type Author = {
 };
 
 export async function Author({ userId }: { userId: number }) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users/${userId}`
   );
