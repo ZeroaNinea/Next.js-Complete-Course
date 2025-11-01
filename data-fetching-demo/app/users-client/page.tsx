070d0d03dcfdcd6b345aefb4018ceee3373cb635
Catgirl <zeroaninea@gmail.com>
2025-11-01 21:27:29 +0400

    /users-client created

---
+++ data-fetching-demo/app/users-client/page.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+export default function UsersClient() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+
+        if (!response.ok) throw new Error('Failed to fetch users.');
+
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unknown error occurred.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchUsers();
+  }, []);
+}
