45cd6c25507e7d265473dd131aefcaf89683f92c
Catgirl <zeroaninea@gmail.com>
2025-11-01 22:49:33 +0400

    the log removed

--- data-fetching-demo/app/users-server/page.tsx
+++ data-fetching-demo/app/users-server/page.tsx
@@ -9,7 +9,6 @@ type User = {
 export default async function UsersServer() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: User[] = await response.json();
-  console.log(users);
 
   return (
     <ul className="space-y-4 p-4">
