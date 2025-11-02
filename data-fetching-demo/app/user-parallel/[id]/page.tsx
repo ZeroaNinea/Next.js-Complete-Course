039ff23c26d02d60f3e34f17cc1de8accbabade3
Catgirl <zeroaninea@gmail.com>
2025-11-02 14:30:54 +0400

    get albums and posts

---
+++ data-fetching-demo/app/user-parallel/[id]/page.tsx
@@ -0,0 +1,28 @@
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+async function getUserPosts(userId: string) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+  return res.json();
+}
+
+async function getUserAlbums(userId: string) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+  );
+  return res.json();
+}
+
+export default async function UserProfile() {}
