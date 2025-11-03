68521af285904751f19b52eda246cb146dd45ae5
Catgirl <zeroaninea@gmail.com>
2025-11-03 22:12:18 +0400

    invalid invocation of .create

--- data-fetching-demo/prisma/seed.ts
+++ data-fetching-demo/prisma/seed.ts
@@ -1,16 +1,29 @@
-import { PrismaClient } from '@/app/generated/prisma/client';
+import { PrismaClient, Prisma } from '@/app/generated/prisma/client';
+
 const prisma = new PrismaClient();
 
-async function main() {
-  const count = await prisma.product.count();
-  if (count === 0) {
-    await prisma.product.createMany({
-      data: [
-        { title: 'Product 1', price: 500, description: 'Description 1' },
-        { title: 'Product 2', price: 700, description: 'Description 2' },
-        { title: 'Product 3', price: 1000, description: 'Description 3' },
-      ],
-    });
+// async function main() {
+//   const count = await prisma.product.count();
+//   if (count === 0) {
+//     await prisma.product.createMany({
+//       data: [
+//         { title: 'Product 1', price: 500, description: 'Description 1' },
+//         { title: 'Product 2', price: 700, description: 'Description 2' },
+//         { title: 'Product 3', price: 1000, description: 'Description 3' },
+//       ],
+//     });
+//   }
+// }
+
+const productData: Prisma.ProductCreateInput[] = [
+  { title: 'Product 1', price: 500, description: 'Description 1' },
+  { title: 'Product 2', price: 700, description: 'Description 2' },
+  { title: 'Product 3', price: 1000, description: 'Description 3' },
+];
+
+export async function main() {
+  for (const u of productData) {
+    await prisma.product.create({ data: u });
   }
 }
 
