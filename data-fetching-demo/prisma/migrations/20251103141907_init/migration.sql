2a6c832887daca28c12067bd95e726579ae1b797
Catgirl <zeroaninea@gmail.com>
2025-11-03 18:40:47 +0400

    ignore migrations

--- data-fetching-demo/prisma/migrations/20251103141907_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "description" TEXT,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
