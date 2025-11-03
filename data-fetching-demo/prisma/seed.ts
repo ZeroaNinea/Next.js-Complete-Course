0160fce86d08cccd3262233ef08bb56bdaa71eab
Catgirl <zeroaninea@gmail.com>
2025-11-03 22:38:28 +0400

    seeding works

--- data-fetching-demo/prisma/seed.ts
+++ data-fetching-demo/prisma/seed.ts
@@ -2,19 +2,6 @@ import { PrismaClient, Prisma } from '../app/generated/prisma/client';
 
 const prisma = new PrismaClient();
 
-// async function main() {
-//   const count = await prisma.product.count();
-//   if (count === 0) {
-//     await prisma.product.createMany({
-//       data: [
-//         { title: 'Product 1', price: 500, description: 'Description 1' },
-//         { title: 'Product 2', price: 700, description: 'Description 2' },
-//         { title: 'Product 3', price: 1000, description: 'Description 3' },
-//       ],
-//     });
-//   }
-// }
-
 const productData: Prisma.ProductCreateInput[] = [
   { title: 'Product 1', price: 500, description: 'Description 1' },
   { title: 'Product 2', price: 700, description: 'Description 2' },
