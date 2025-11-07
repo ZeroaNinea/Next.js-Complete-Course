e9c78b2592a9ff9b05a12eaca7d1c8f4a13030fa
Catgirl <zeroaninea@gmail.com>
2025-11-07 16:33:09 +0400

    product creating implemented

--- data-fetching-demo/app/products-db-create/page.tsx
+++ data-fetching-demo/app/products-db-create/page.tsx
@@ -1,35 +1,64 @@
+'use client';
+
+import { FormState, createProduct } from '@/actions/products';
+import { Submit } from '@/components/submit';
+import { useActionState } from 'react';
+
 export default function AddProductPage() {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const [state, formAction] = useActionState(createProduct, initialState);
+
   return (
-    <form className="p-4 space-y-4 max-w-96">
-      <label className="text-gray-700">
-        Title
-        <input
-          type="text"
-          className="block w-full p-2 text-black border rounded"
-          name="title"
-        />
-      </label>
-      <label className="text-gray-700">
-        Price
-        <input
-          type="number"
-          className="block w-full p-2 text-black border rounded"
-          name="price"
-        />
-      </label>
-      <label className="text-gray-700">
-        Description
-        <textarea
-          className="block w-full p-2 text-black border rounded"
-          name="description"
-        />
-      </label>
-      <button
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
+      <div>
+        <label className="text-white">
+          Title
+          <input
+            type="text"
+            className="block w-full p-2 text-black border rounded"
+            name="title"
+          />
+        </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Price
+          <input
+            type="number"
+            className="block w-full p-2 text-black border rounded"
+            name="price"
+          />
+        </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
+      </div>
+      <div>
+        <label className="text-white">
+          Description
+          <textarea
+            className="block w-full p-2 text-black border rounded"
+            name="description"
+          />
+        </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
+      </div>
+      {/* <button
         type="submit"
         className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"
+        disabled={isPending}
       >
-        Add Product
-      </button>
+        Submit
+      </button> */}
+      <Submit />
     </form>
   );
 }
